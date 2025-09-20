7791d16b1da3f274629a90c98e571b06dbc43700
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-09-20 22:40:30 +0200

    Nostalgia

---
+++ Time.java
@@ -0,0 +1,9 @@
+public class Time {
+    private int hour;
+    private int minute;
+    private double second;
+
+    public static void main(String [] args) {
+        System.out.println("Tana hosh");
+    }
+}
