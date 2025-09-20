230c65f4b4f25d3991cef81610b0a27394683a70
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-09-20 23:17:08 +0200

    Going the human friendly route

---
+++ Human.java
@@ -0,0 +1,21 @@
+public class Human {
+    private int age;
+    private String name;
+    private double weight;
+
+    public Human() {
+        this.age = age;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public Human(int age, String name, double weight) {
+        this.age = age;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("How To Think Like A Computer Scientist");
+    }
+}
