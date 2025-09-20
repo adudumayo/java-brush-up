35824fe8b1517c8e5e35059df4852225d85bb846
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-09-20 22:44:15 +0200

    I call it static constructor

--- Time.java
+++ Time.java
@@ -3,7 +3,13 @@ public class Time {
     private int minute;
     private double second;
 
+    public Time() {
+        this.hour = 0;
+        this.minute = 0;
+        this.second = 0.0;
+    }
+
     public static void main(String [] args) {
-        System.out.println("Tana hosh");
+        System.out.println("How To Think Like A Computer Scientist");
     }
 }
